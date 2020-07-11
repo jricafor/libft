10574d3093eb329bc8cd9f473d074ff99ef72786
jricafor <jricafor@student.42.fr>
2020-07-11 16:03:36 +0000

    update

--- libft.h
+++ libft.h
@@ -38,7 +38,7 @@ int				ft_strncmp(char *s1, char *s2, size_t n);
 size_t			ft_strlcpy(char *dest, char *src, size_t size);
 size_t			ft_strlcat(char *dest, char *src, size_t size);
 char			*ft_strnstr(const char *s1, const char *s2, size_t len);
-int				ft_atoi(char *str);
+int				ft_atoi(const char *str);
 void			*ft_calloc(unsigned int nmemb, size_t size);
 char			*ft_strdup(char *src);
 char			*ft_substr(char const *s, unsigned int start, size_t len);
